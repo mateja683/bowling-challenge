352126baffd8a21dd7b7e535932c95636fc3606a
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-11-19 16:05:39 +0000

    calcBonusScore started

--- scorecard.go
+++ scorecard.go
@@ -1,31 +1,30 @@
 package main
 
 import ("fmt"
-        "math/rand"
-        "time"
+        // "math/rand"
+        // "time"
       )
 
   var scorecard = []int{}
   // var frames = [][]int{}
-  var new = [][]int{}
   var framesTotals = []int{}
-  var frame = []int{}
-  var bowl1 int
-  var bowl2 int
+  // var frame = []int{}
+  // var bowl1 int
+  // var bowl2 int
 
-func bowl() {
-  for i :=1; i <=10; i++ {
-    rand.Seed(time.Now().Unix())
-    bowl1 = rand.Intn(11)
-  if bowl1 == 10 {
-    bowl2 = 0
-  } else {
-    rand.Seed(time.Now().Unix())
-    bowl2 = rand.Intn(11-bowl1)
-  }
-  pushScores(bowl1, bowl2)
-  }
-}
+// func bowl() {
+//   for i :=1; i <=10; i++ {
+//     rand.Seed(time.Now().Unix())
+//     bowl1 = rand.Intn(11)
+//   if bowl1 == 10 {
+//     bowl2 = 0
+//   } else {
+//     rand.Seed(time.Now().Unix())
+//     bowl2 = rand.Intn(11-bowl1)
+//   }
+//   pushScores(bowl1, bowl2)
+//   }
+// }
 
 func pushScores(bowl1, bowl2 int) {
   scorecard = append(scorecard, bowl1, bowl2)
@@ -44,11 +43,10 @@ func createFrames(scorecard []int) [][]int {
   for i, idx := 0, 0; i < 10; i, idx = i + 1, idx + 2 {
     frames[i] = scorecard[idx:idx+2]
   }
-  // frames = append(frames)
   return frames
 }
 
-func calcBonusScore(frames [][]int) ([]int) {
+func calcFramesTotal(frames [][]int) []int {
   for i := 0; i < len(frames); i++ {
     frame := frames[i]
     sum := 0
@@ -58,18 +56,25 @@ func calcBonusScore(frames [][]int) ([]int) {
       framesTotals = append(framesTotals, sum)
   }
   return framesTotals
-
 }
 
+func calcBonusScore(framesTotals, scorecard []int) {
+  for i := 0; i < len(framesTotals); i++ {
+    if framesTotals[i] == 10 {
+      if scorecard[i*2] == 10 {
+        fmt.Println("strike")
+      } else {
+        fmt.Println("spare")
+      }
+    }
+  }
+}
 
-// func appendFunc() {
-//
-// }
 
 
 func main () {
   // bowl()
-  pushScores(2, 5)
+  pushScores(10, 0)
   pushScores(3, 4)
   pushScores(5, 5)
   pushScores(10, 0)
@@ -83,6 +88,6 @@ func main () {
   fmt.Println(scorecard)
   fmt.Println(addScores(scorecard))
   fmt.Println(createFrames(scorecard))
-  fmt.Println(calcBonusScore(createFrames(scorecard)))
-  // fmt.Println(frames)
+  fmt.Println(calcFramesTotal(createFrames(scorecard)))
+  calcBonusScore(framesTotals, scorecard)
 }
